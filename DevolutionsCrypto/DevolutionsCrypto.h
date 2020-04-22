84f4a8dc102f99d51e14529d04447d1b4cacffda
Sebastien Aubin <spicheaubin@devolutions.net>
2020-04-22 11:08:25 -0400

    Initial project setup

---
+++ DevolutionsCrypto/DevolutionsCrypto.h
@@ -0,0 +1,19 @@
+//
+//  DevolutionsCrypto.h
+//  DevolutionsCrypto
+//
+//  Created by Sebastien Aubin on 2020-04-22.
+//  Copyright © 2020 Devolutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DevolutionsCrypto.
+FOUNDATION_EXPORT double DevolutionsCryptoVersionNumber;
+
+//! Project version string for DevolutionsCrypto.
+FOUNDATION_EXPORT const unsigned char DevolutionsCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DevolutionsCrypto/PublicHeader.h>
+
+
